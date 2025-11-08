9b5d578bf8f750d88636892fc4175b5fbb407ca4
KailHet <KailHet@mail.ru>
2025-11-08 20:36:23 +0500

    upd

--- src/bot/index.js
+++ src/bot/index.js
@@ -1,4 +1,8 @@
 import {bot} from './index.js'
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
 function initialSessionData() {
   return { command: null };
